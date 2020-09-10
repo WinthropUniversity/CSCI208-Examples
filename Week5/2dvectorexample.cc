7e61a97a62c8357576010b590276db3065395918
R. Paul Wiegand <rpwiegand@gmail.com>
2020-09-09 20:12:01 -0400

    Added a 2-dimensional vector example

---
+++ Week5/2dvectorexample.cc
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+  
+int main() { 
+  int nrows=0;
+  int ncols=0;
+
+  cout << endl;
+
+  cout << "Enter the number of rows:    ";
+  cin >> nrows;
+
+  cout << "Enter the number of columns: ";
+  cin >> ncols;
+
+  // Declare a vectors of vectors of int, and initialize
+  // its sizes
+  vector< vector<int> > matrix(nrows, vector<int>(ncols));
+
+  // Mark the corners!
+  matrix[0][0] = 1; 
+  matrix[nrows-1][ncols-1] = 1;
+
+  cout << endl;
+ 
+  // Displaying the integer matrix
+  for (int idx = 0; idx < matrix.size(); idx++) { 
+    for (int jdx = 0; jdx < matrix[idx].size(); jdx++) 
+      cout << matrix[idx][jdx] << " "; 
+    cout << endl; 
+  } 
+
+  cout << endl;
+
+  // Let the OS know everything is fine  
+  return 0; 
+} 
+
