51c44f91863d5f7f13a80a993fd2abac6189f968
rpwiegand <rpwiegand@gmail.com>
2021-02-10 12:09:15 -0500

    Added array example to playing around source file

--- Week5/playingaround.cc
+++ Week5/playingaround.cc
@@ -14,11 +14,23 @@ int main() {
   countptr = &count;
   cout << "5 countptr = " << countptr << endl;
 
-  delete countptr;
+  /*delete countptr;
   cout << "6 countptr = " << countptr << endl;
-  
+
   (*countptr) = 100;
-  cout << "7 count  = " << count << endl;
+  cout << "7 count  = " << count << endl;*/
+
+  int *myarray;
+  int arraysize;
+
+  cout << "How big?";
+  cin >> arraysize;
+
+  myarray = new int [arraysize];
+  myarray[arraysize-1] = arraysize;
+  cout << "myarray[arraysize-1] = " << myarray[arraysize-1] << endl;
+
+  delete [] myarray;
 
   return 0;
 }
