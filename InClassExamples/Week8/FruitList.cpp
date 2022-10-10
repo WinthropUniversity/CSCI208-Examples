640c83f63aeaef0ea67bb3942d72e9c4122d624d
rpwiegand <rpwiegand@gmail.com>
2022-10-10 11:47:08 -0400

    Testing the input loop

--- InClassExamples/Week8/FruitList.cpp
+++ InClassExamples/Week8/FruitList.cpp
@@ -10,6 +10,14 @@ int main() {
 
   cout << "Dummy Header: " << dummyHeader << endl;
   cout << "Header's Fruit:  [" << dummyHeader->GetFruitName() << "]" << endl;
+
+  string userFruitName;
+
+  do {
+    cout << "Name a fruit (or 'DONE' if finished): ";
+    cin >> userFruitName;
+  } while (userFruitName != "DONE");
+
   // The program ran correctly
   return 0;
 }
