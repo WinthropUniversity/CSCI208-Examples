8fdc4a37a13cef5a112be51951b6fba663fd7039
rpwiegand <rpwiegand@gmail.com>
2022-10-10 12:00:56 -0400

    Finished input loop and output loop

--- InClassExamples/Week8/FruitList.cpp
+++ InClassExamples/Week8/FruitList.cpp
@@ -16,8 +16,26 @@ int main() {
   do {
     cout << "Name a fruit (or 'DONE' if finished): ";
     cin >> userFruitName;
+
+    FruitNode *newNode = new FruitNode(userFruitName);
+
+    // Save off dummy header's next ptr
+    FruitNode *oldRealFirstNode = dummyHeader->GetNextNode()
+
+    // Connect the dummy header to the new node
+    dummyHeader->SetNextNode(newNode);
+
+    // Connect the new node to the old first node
+    newNode->SetNextNode(oldRealFirstNode);
   } while (userFruitName != "DONE");
 
+
+  cout << endl << "Here are your fruits:" << endl;
+  FruitNode *current = dummyHeader->GetNextNode();
+  while (current != NULL) {
+    cout << "  " << current->GetFruitName() << endl;
+    current = current->GetNextNode();
+  }
   // The program ran correctly
   return 0;
 }
