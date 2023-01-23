2b7f466e6d46b98437e9fc159138224e1a4aa549
rpwiegand <rpwiegand@gmail.com>
2023-01-23 11:55:01 -0500

    Altered makefile to separate comp and link

--- Week1/hello.cc
+++ Week1/hello.cc
@@ -1,6 +1,7 @@
 #include "hello.h"
 
 void printHello() {
-  std::cout << "Hello World! This is Dr. Wiegand!" << std::endl;
+  std::cout << "Hello World! This is Dr. Wiegand!  It's awesome!!" << std::endl;
+  std::cout << "Goodbye!" << std::endl;
 }
 
