e1d530d50b943803b28ca9b17a938fa80ec4b199
rpwiegand <rpwiegand@gmail.com>
2023-04-24 11:40:36 -0400

    switched the a and b to match our class example

--- InClassExamples/Week15/printingstuff.cc
+++ InClassExamples/Week15/printingstuff.cc
@@ -16,17 +16,17 @@ void printthis(int n, string spaces) {
   cout << spaces << "---> Entering the frame where n = " << n << endl;
   if (n <= 0) cout << spaces << "X" << " :: frame n = " << n << endl;
   else {
-    cout << spaces << "b" << " :: frame n = " << n << endl;
+    cout << spaces << "a" << " :: frame n = " << n << endl;
     printthis(n-1, spaces + "  ");
     cout << spaces << "-" << " :: frame n = " << n << endl;
     printthis(n-2, spaces + "  ");
-    cout << spaces << "a" << " :: frame n = " << n << endl;
+    cout << spaces << "b" << " :: frame n = " << n << endl;
   }
   cout << spaces << "<--- Leaving the frame where n = " << n << endl;
 }
 
 int main() {
-    printthis(3, "  ");
+    printthis(4, "  ");
     cout << endl;
 
     return 0;
