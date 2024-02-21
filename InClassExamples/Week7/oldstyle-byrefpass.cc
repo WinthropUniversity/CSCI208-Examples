624461dbca2d70fbf2e785ea0ce66139d778da75
R. Paul Wiegand <rpwiegand@gmail.com>
2024-02-21 11:24:14 -0500

    Commented example use of ptr

--- InClassExamples/Week7/oldstyle-byrefpass.cc
+++ InClassExamples/Week7/oldstyle-byrefpass.cc
@@ -14,6 +14,11 @@ void DoSomethingToName(string &name) {
 }
 
 int main() {
+
+    //double* xptr = new double; // Declare and allocate the ptr
+    //*xptr = 3.14;  // Assign a value to the spot in memory where that ptr is pointing
+    //delete xptr;   // Deallocate the ptr
+
     string name = "Nothing";
 
     cout << "What is your name? ";
